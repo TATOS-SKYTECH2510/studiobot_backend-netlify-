c0e0d04a564b341c7eb65adf09883ef6be2a01f0
skytech2510 <skytech2510@gmail.com>
2025-03-18 14:40:23 +0800

    update

--- src/lib/twilio.ts
+++ src/lib/twilio.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const accountSid = process.env.TWILIO__SID;
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const phoneNumberType = 'TollFree';
 export const buyTwilioPhoneNumber = async () => {
