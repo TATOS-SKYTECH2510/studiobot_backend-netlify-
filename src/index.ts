a846e413969e870e39823ef9e9bdf5dde8703337
skytech2510 <skytech2510@gmail.com>
2025-03-23 13:13:34 +0800

    feat: netlify hosting setting

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import router from './router';
 import dotenv from 'dotenv';
 import cors from "cors";
+import serverless from "serverless-http"
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,7 +10,4 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // Middleware to parse JSON requests
 app.use(cors())
 app.use('/api', router);
-
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
-});  
+export const handler = serverless(app);
